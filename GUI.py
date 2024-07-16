35d10e94225731c7f0f0af72dfd02658055ae4eb
IbrahAbd <ibrahim.abdou2121@gmail.com>
2024-07-16 17:22:42 +0200

    Summoner Spells now shown on GUI and Rune planning shown on GUI.

--- GUI.py
+++ GUI.py
@@ -1,31 +1,52 @@
 import sys
+import time
 from OPGGparser import *
 from PyQt5 import QtWidgets, QtGui, QtCore, uic
 from PyQt5.QtGui import QPixmap
-
-roles = ["Top","Jungle","ADC","Mid","Support"]
-champ = "Zac" 
-role = roles[0]
-
-rates = output(champ,role)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('GUI.ui', self) 
-        
-        self.display_image(f'Images\{champ}.png')
 
+        champ = "Jhin" 
+
+        start_time = time.time()
+        rates = output(champ)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        role = rates['role']
         font = QtGui.QFont()
         font.setPointSize(40)  
-        self.name.setText(champ)
-        self.name.setFont(font)
-
+        
         pixmap = QPixmap(f'Roles\{role}.png')
         self.roleIMG.setPixmap(pixmap)
         self.roleIMG.setPixmap(pixmap.scaled(self.roleIMG.size(), aspectRatioMode=True))
         self.roleIMG.resize(pixmap.width(), pixmap.height())
 
+        self.display_image(f'Images\{champ}.png')
+
+        if (champ.find(" ") == False):
+            champName = champ.split(" ")
+            champ = champName[0] + " " + champName[1]
+            print(champ)
+
+        self.name.setText(champ)
+        self.name.setFont(font)
+
+        sumSpell1 = rates['SumSpell1'][0]
+        sumSpell2 = rates['SumSpell2'][0]
+
+        pixmapSpell1 = QPixmap(f'SummonerSpells\{sumSpell1}.png')
+        self.Summoner1.setPixmap(pixmapSpell1)
+        self.Summoner1.setPixmap(pixmapSpell1.scaled(self.Summoner1.size(), aspectRatioMode=True))
+        self.Summoner1.resize(pixmapSpell1.width(), pixmapSpell1.height())
+
+        pixmapSpell2 = QPixmap(f'SummonerSpells\{sumSpell2}.png')
+        self.Summoner2.setPixmap(pixmapSpell2)
+        self.Summoner2.setPixmap(pixmapSpell2.scaled(self.Summoner2.size(), aspectRatioMode=True))
+        self.Summoner2.resize(pixmapSpell2.width(), pixmapSpell2.height())
 
         font2 = QtGui.QFont()
         font2.setPointSize(20)  
@@ -43,7 +64,14 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.role.setText(role)
         self.role.setFont(font2)
-        
+
+        #pixmapMainRune = QPixmap(f'SummonerSpells\{sumSpell2}.png')
+        #self.Summoner2.setPixmap(pixmapMainRune)
+        #self.Summoner2.setPixmap(pixmapMainRune.scaled(self.Summoner2.size(), aspectRatioMode=True))
+        #self.Summoner2.resize(pixmapMainRune.width(), pixmapMainRune.height())
+
+
+        print(f"Elapsed time: {elapsed_time} seconds\n")
 
     def display_image(self, image_path):
         pixmap = QtGui.QPixmap(image_path)
@@ -56,9 +84,8 @@ class MainWindow(QtWidgets.QMainWindow):
             print(f"Error: Unable to load image from {image_path}")
 
 if __name__ == "__main__":
+    
     app = QtWidgets.QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec_())
-
-    print("dog")
--- OPGGparser.py
+++ OPGGparser.py
@@ -1,48 +1,82 @@
-import requests
-import time
 from bs4 import BeautifulSoup
+import requests
 
-# Function to get the HTML content of the page
+SummonerSpellSRCs = [
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerFlash.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerSmite.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerBarrier.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerHaste.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerHeal.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerDot.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerTeleport.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.13.1/img/spell/SummonerExhaust.webp",
+
+]
+RuneTypes = ["Domination","Sorcery","Inspiration","Precision","Resolve"]
+
 def get_page_html(url):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
     response = requests.get(url, headers=headers)
-    time.sleep(10)
     if response.status_code == 200:
         return response.text
     else:
         return None
 
+
 def extract_value(text):
     return text.split('%')[0] if '%' in text else text
 
-# Function to parse the HTML content and extract Jhin's win rate and pick rate
 def parse_rates(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "lxml")
     combo = {}
-    
-    # The winR below is to find the first instance of win-rate. 
-    # They are many weird names and this is the easiest way to fix the problem I think?
-    winR = (soup.find(lambda tag: tag.name == 'div' and 'win-rate' in tag.get('class', []))).get_text().strip()
-    pickR = (soup.find('div', class_='pick-rate')).get_text().strip()
-    banR = (soup.find('div', class_='ban-rate')).get_text().strip()
-    
-    combo['win_rate'] = extract_value(winR) + '%'
-    combo['pick_rate'] = extract_value(pickR) + '%'
-    combo['ban_rate'] = extract_value(banR) + '%'
+
+    try:
+        win_rate = soup.find('div', class_='win-rate').get_text().strip()
+        pick_rate = soup.find('div', class_='pick-rate').get_text().strip()
+        ban_rate = soup.find('div', class_='ban-rate').get_text().strip()
+        role = soup.find('span', class_='champion-title').get_text().strip().split()
+        mainRune = soup.find('div', class_='perk-style-title').get_text().strip()
+        secondaryRune = soup.find('div', class_='perk-style-title').get_text().strip()
+
+        combo['win_rate'] = extract_value(win_rate) + '%'
+        combo['pick_rate'] = extract_value(pick_rate) + '%'
+        combo['ban_rate'] = extract_value(ban_rate) + '%'
+        position = role[2]
+        combo['role'] = extract_value(position[:-1])
         
-    return combo
+        count = 0
+        for i in range (2):
+            for src in SummonerSpellSRCs:
+                img = soup.find('img', {'src': src})
+                if img:
+                    alt_text = img['alt']
+                    if count == 0:
+                        combo['SumSpell1'] = alt_text.split()[-1:]
+                        count += 1
+                    elif count == 1:
+                        combo['SumSpell2'] = alt_text.split()[-1:]
 
+        runes = soup.find_all('div', class_='perk-style-title')
+        combo['MainRune'] = runes[0].text
+        combo['SecondaryRune'] = runes[1].text
 
-def output(champ,role):
-    url = f'https://u.gg/lol/champions/{champ}/build/{role}?rank=overall'
+        return combo
+    
+    except AttributeError as e:
+        print(f"Error parsing rates: {e}")
+        return None
+
+def output(champ):
+    url = f'https://u.gg/lol/champions/{champ}/build?rank=overall'
     html = get_page_html(url)
+    
     if html:
         rates = parse_rates(html)
         if rates:
             return rates
         else:
-            print("Could not find the win rate and pick rate on the page.")
+            print("Could not find rates on the page.")
     else:
         print("Failed to retrieve the page.")
