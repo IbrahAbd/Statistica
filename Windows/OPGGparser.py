8abb99a138a0c78928df19f4b0b036bcb499959e
IbrahAbd <ibrahim.abdou2121@gmail.com>
2024-07-20 15:53:52 +0200

    Version 0.1

---
+++ Windows/OPGGparser.py
@@ -0,0 +1,144 @@
+from bs4 import BeautifulSoup
+import requests
+import asyncio
+import aiohttp
+
+SummonerSpellSRCs = [
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerFlash.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerSmite.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerBarrier.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerHaste.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerHeal.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerDot.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerTeleport.webp",
+    "https://static.bigbrain.gg/assets/lol/riot_static/14.14.1/img/spell/SummonerExhaust.webp",
+]
+
+RuneTypes = ["Domination","Sorcery","Inspiration","Precision","Resolve"]
+async def get_page_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                return await response.text()
+            else:
+                return None
+
+
+def extract_value(text):
+    return text.split('%')[0] if '%' in text else text
+
+async def parse_rates(html):
+    soup = BeautifulSoup(html, "lxml")
+    combo = {}
+
+    try:
+        win_rate = soup.find('div', class_='win-rate').get_text().strip()
+        pick_rate = soup.find('div', class_='pick-rate').get_text().strip()
+        ban_rate = soup.find('div', class_='ban-rate').get_text().strip()
+        role = soup.find('span', class_='champion-title').get_text().strip().split()
+        mainRune = soup.find('div', class_='perk-style-title').get_text().strip()
+
+        combo['win_rate'] = extract_value(win_rate) + '%'
+        combo['pick_rate'] = extract_value(pick_rate) + '%'
+        combo['ban_rate'] = extract_value(ban_rate) + '%'
+        position = role[2]
+        combo['role'] = extract_value(position[:-1])
+        
+        count = 0
+
+        runes = soup.find_all('div', class_='perk-style-title')
+        combo['MainRuneName'] = runes[0].text
+        combo['SecondaryRuneName'] = runes[1].text
+        
+        mainRuneHTML = soup.find('div',class_="perk keystone perk-active")
+        perk_name1 = mainRuneHTML.img['alt'].split()
+        mainRune = ' '.join(perk_name1[2:])
+        combo['MainPrimaryRune']  = mainRune
+
+        mainSecondaryRuneHTML = soup.find_all('div',class_="perk perk-active")
+
+        perk_names = []
+
+        for rune in mainSecondaryRuneHTML:
+            alt_text = rune.img['alt'].split()
+            minorRune = (' '.join(alt_text[2:])).replace(" ", "").replace(":","")
+            perk_names.append(minorRune)
+
+
+        combo['MinorRunes'] = perk_names
+
+        shard_names = []
+        shardRuneHTML = soup.find_all('div',class_="shard shard-active")
+        for shard in shardRuneHTML:
+            altText = shard.img['alt']
+            shard_names.append(altText)
+
+        combo['Shards'] = shard_names
+            
+        for i in range (2):
+            for src in SummonerSpellSRCs:
+                img = soup.find('img', {'src': src})
+                if img:
+                    alt_text = img['alt']
+                    if count == 0:
+                        combo['SumSpell1'] = alt_text.split()[-1:]
+                        count += 1
+                    elif count == 1:
+                        combo['SumSpell2'] = alt_text.split()[-1:]
+
+
+        return combo
+    
+    except AttributeError as e:
+        print(f"Error parsing rates: {e}")
+        return None
+
+async def output(champ):
+    url = f'https://u.gg/lol/champions/{champ}/build?rank=overall'
+    html = await get_page_html(url)
+    
+    if html:
+        rates = await parse_rates(html)
+        if rates:
+            return rates
+        else:
+            print("Could not find rates on the page.")
+    else:
+        print("Failed to retrieve the page.")
+
+async def counters(champ):
+    url = f'https://u.gg/lol/champions/{champ}/counter?rank=overall'
+    html = await get_page_html(url)
+    counters = {}
+
+    if html:
+        soup = BeautifulSoup(html, "lxml")
+        i = 0
+        outer_divs = soup.find_all('div', class_='col-2')
+        if outer_divs:
+            for champion in outer_divs:
+                inner_div1 = champion.find('div', class_='champion-name')
+                if inner_div1:
+                    name = inner_div1.get_text().strip()
+                    counters[f"champ{i}"] = {"name": name}
+                    i += 1
+
+        j = 0
+        outer_div2 = soup.find_all('div', class_='col-3')
+        if outer_div2:
+            for div in outer_div2:
+                winRate = div.find('div', class_='win-rate').get_text().strip()
+                if winRate:
+                    if j > 9:
+                        counters[f"champ{j}"]["lose_rate"] = winRate
+                    else:
+                        counters[f"champ{j}"]["win_rate"] = winRate
+                    j += 1
+
+        if counters:
+            return counters
+        else:
+            print("Could not find rates on the page.")
+    else:
+        print("Failed to retrieve the page.")
+
---
+++ Windows/startScreen.py
@@ -0,0 +1,124 @@
+import sys
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtWidgets import QMainWindow, QStackedWidget, QLineEdit
+from PyQt5.QtCore import QThread, pyqtSignal
+from loadingScreen import MainWindow3
+from GUI import MainWindow2
+
+class ScraperThread(QThread):
+    scraping_done = pyqtSignal(str)  # Pass the champion name as a signal
+
+    def __init__(self, champ):
+        super().__init__()
+        self.champ = champ
+
+    def run(self):
+        self.scraping_done.emit(self.champ)
+
+class MainWindow1(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('StartScreen.ui', self)
+
+        self.stacked_widget = self.findChild(QStackedWidget, 'stackedWidget')
+        self.lineEdit = self.findChild(QLineEdit, 'lineEdit')
+
+        self.loading_screen = None
+        self.main_window_2 = None
+
+        self.initialize_line_edit()
+
+
+    def initialize_line_edit(self):
+        champions = [
+            "Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "AurelionSol", "Aurora",
+            "Azir", "Bard", "Belveth", "Blitzcrank", "Brand", "Braum", "Briar", "Caitlyn", "Camille", "Cassiopeia", "ChoGath", "Corki",
+            "Darius", "Diana", "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora",
+            "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen", "Hecarim", "Heimerdinger", "Hwei",
+            "Illaoi", "Irelia", "Ivern", "Janna", "JarvanIV", "Jax", "Jayce", "Jhin", "Jinx", "Kaisa", "Kalista",
+            "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Khazix", "Kindred", "Kled",
+            "KogMaw", "Ksante", "Leblanc", "LeeSin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Naafiri",
+            "Malzahar", "Maokai", "MasterYi", "MissFortune", "Milio", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus",
+            "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu&Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana",
+            "Quinn", "Rakan", "Rammus", "RekSai", "Rell", "RenataGlasc", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira",
+            "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Smolder",
+            "Sona", "Soraka", "Swain", "Sylas", "Syndra", "TahmKench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh",
+            "Tristana", "Trundle", "Tryndamere", "TwistedFate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar",
+            "Velkoz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "XinZhao",
+            "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"
+        ]
+            
+        championsLower = [champ.lower() for champ in champions]
+
+        for i, champ in enumerate(champions, 1):
+            button_name = f"pushButton_{i}"
+            button = getattr(self, button_name, None)
+            if button:
+                button.setStyleSheet(f"background-image: url('Images/{champ}.png');")
+                button.setText("")
+                button.setObjectName(champ)
+                button.clicked.connect(self.on_button_clicked)
+
+        completer = QtWidgets.QCompleter(champions + championsLower)
+        self.lineEdit.setCompleter(completer)
+        self.lineEdit.returnPressed.connect(self.on_return_pressed)
+
+    def on_button_clicked(self):
+        button = self.sender()  
+        champ_name = button.objectName()
+        self.lineEdit.setText(champ_name) 
+        self.on_return_pressed()
+
+    def on_return_pressed(self):
+        champ = self.lineEdit.text()
+        champ = champ[0].upper() + champ[1:]
+        self.lineEdit.clear()
+
+        self.scraper_thread = ScraperThread(champ)
+        self.scraper_thread.scraping_done.connect(self.on_scraping_done)
+        self.scraper_thread.start()
+
+    def on_scraping_done(self, champ):
+        # Create and show the loading screen
+        self.loading_screen = MainWindow3("gifs")
+        self.stacked_widget.addWidget(self.loading_screen)
+        self.stacked_widget.setCurrentWidget(self.loading_screen)
+
+        # Create MainWindow2 and connect its signal
+        self.main_window_2 = MainWindow2(champ)
+        self.main_window_2.results_shown.connect(self.on_results_shown)
+        self.main_window_2.back_to_main.connect(self.handle_back_to_main)
+
+        # Wait for results and switch to MainWindow2
+        self.wait_for_results_shown()
+
+    def wait_for_results_shown(self):
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.check_if_results_shown)
+        self.timer.start(100)  # Check every 100 ms
+
+    def check_if_results_shown(self):
+        if self.main_window_2 and hasattr(self.main_window_2, 'results_shown_emitted') and self.main_window_2.results_shown_emitted:
+            self.timer.stop()
+            if not self.stacked_widget.widget(self.stacked_widget.indexOf(self.main_window_2)):
+                self.stacked_widget.addWidget(self.main_window_2)
+            self.stacked_widget.setCurrentWidget(self.main_window_2)
+
+    def on_results_shown(self):
+
+        self.main_window_2.results_shown_emitted = True
+
+    def handle_back_to_main(self):
+        if self.main_window_2:
+            self.stacked_widget.removeWidget(self.main_window_2)
+            self.main_window_2.close()
+            self.main_window_2 = None
+
+        self.stacked_widget.setCurrentIndex(0)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = MainWindow1()
+    mainWindow.show()
+    sys.exit(app.exec_())
